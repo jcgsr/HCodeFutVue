b307e12d05ff79a698b43be67a84976562098a66
jcgsr <cabralguerra@gmail.com>
2020-06-21 12:53:43 -0300

    1st commit

---
+++ hcode-fut/src/components/HcodeSection.vue
@@ -0,0 +1,16 @@
+<template>
+   <HcodeSectionBanner />
+</template>
+
+<script>
+import HcodeSectionBanner from './HcodeSectionBanner'
+export default {
+   components: {
+      HcodeSectionBanner
+   }
+}
+</script>
+
+<style scoped>
+
+</style>
